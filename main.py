39288b9886563bf4c26be45f3fd7fc37946a2600
nicotea <stabilotea@gmail.com>
2023-04-19 18:31:10 +1000

    Merci Raph

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from store_database import CustomerForm
 a = CustomerForm()
 a.create_list("store database")
 a.display_name()
-a.get_whole_customer_order()
+a.get_whole_customer_order() # Merci Raph
