1e025a0009bafd713734c37fe6715f65dd506657
Raphael Heo-Hsien-Kai <raphaelhhk@gmail.com>
2023-04-19 18:31:12 +1000

    Added thank you message to Nico

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ from store_database import CustomerForm
 a = CustomerForm()
 a.create_list("store database")
 a.display_name()
-a.get_whole_customer_order()
+a.get_whole_customer_order()        ### Merci Nico pour l'aide
+
